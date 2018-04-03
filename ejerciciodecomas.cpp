2c0531297a96d6ff3e393f253c9204b5ab14dd9f
alduma <37373309+alduma@users.noreply.github.com>
2018-04-03 09:19:28 -0500

    Update ejerciciodecomas.cpp

--- ejerciciodecomas.cpp
+++ ejerciciodecomas.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x;
+
+    cin>>x;
+    for(int i=10000;i>=1;i=i/10){
+        int tmp=x/i;
+        cout<<tmp<<","<<"\t";
+        x=x%i;
+    }
     return 0;
-}
+    }
