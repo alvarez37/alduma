980b158f26d9dd7017b972d8d24d84589ec672a1
alduma <37373309+alduma@users.noreply.github.com>
2018-04-04 09:14:26 -0500

    Add files via upload

---
+++ ejercicio2(del 1 hasta n).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int n=0;
+
+    cin>>x;
+   while(n<=x){
+
+
+        cout<<n<<","<<"\t";
+        n=n+1;
+
+    }
+    return 0;
+    }
